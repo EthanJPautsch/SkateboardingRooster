2d959ddc3cdb1dc730d6fa604ee3c8e00eb02009
Ethan P <ethan.pautsch@gmail.com>
2024-10-16 18:40:03 -0400

    Basic Setup
    Temp hill created. Board auto moves forward, acceleration sort of works, needs some tweaks. Player is able to move left and right.

---
+++ Skateboarding Rooster/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer();
+        if(Input.GetButton("Vertical"))
+        {
+            moveSpeed = moveSpeed + (1 * Time.deltaTime);
+        }
+    }
+    
+    void movePlayer()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        move.Normalize();
+        Vector3 moveVelocity = move * moveSpeed;
+        moveVelocity.y = rb.velocity.y;
+        rb.velocity = moveVelocity;
+    }
+}
